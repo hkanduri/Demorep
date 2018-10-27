aa1fb594d16daceb6ed2a25cee6c57996f5cd9b2
sandy <sandy@sandy-PC>
2018-10-27 16:55:51 +0530

    first commit

---
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/Testc1.java
@@ -0,0 +1,21 @@
+package com.crm.qa.testcases;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.redbus.in/");
+		driver.manage().window().maximize();
+			}
+
+}
